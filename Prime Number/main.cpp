6820aec8b2a277d18de9173f4a35ae12fa944233
Akshat Marathe <marathe.akshat@live.com>
2022-12-24 22:27:21 +0530

    changes in prime number solution

--- Prime Number/main.cpp
+++ Prime Number/main.cpp
@@ -7,7 +7,7 @@ class Solution{
     bool isPrime(int number){
         if(number==2 || number==3)
             return true;
-        if(number==0 || number==1 || number%2==0 || number%3==0)
+        if(number<=1 || number%2==0 || number%3==0)
             return false;
         for(int i=5;i*i<=number;i+=6)//incrementing number by 6 as we have already checked conditions with 2 and 3
             if(number%i==0 || number%(i+2)==0)//Checking two conditions
