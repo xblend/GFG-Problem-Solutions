a696cd43cbe1e7e01569fe37bce80bd957cc4711
Akshat Marathe <marathe.akshat@live.com>
2023-01-01 18:40:06 +0530

    Changes to make function tail recursive

--- Print numbers from 1 to N/main.cpp
+++ Print numbers from 1 to N/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 class Solution{
     public:
-    void print1toN(int N){
+    void print1toN(int N, int K){
         if(N==0)
             return;
+        /*
         print1toN(N-1);
         cout << N << " ";
-        
+        */
+       //Making code tail recursive so that code takes Theta(1) auxilary space
+       cout << K << " ";
+       print1toN(N-1,K+1);
     }
 };
 
@@ -19,6 +23,6 @@ void main(){
     cout << "Enter the number: ";
     cin >> number;
 
-    sol.print1toN(number);
+    sol.print1toN(number,1);
     cout << endl;
 }
