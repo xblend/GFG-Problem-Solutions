483777a4db3457b908a3760124bbcfa59980b5bd
Akshat Marathe <marathe.akshat@live.com>
2023-01-01 19:17:29 +0530

    Making function tail recursive

--- Factorial/main.cpp
+++ Factorial/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 class Solution{
     public:
+    /*
     long long int factorial(long long int number){
         if(number==0)
             return 1;
-        return number*factorial(number-1);
+        return number*factorial(number-1);//This is not tail recursive because the function call stack needs to be stored inorder to need the return value.
     }
+    */
+   //Making factorial code tail recursive
+   //A tail recursive function take Theta(1) auxilarly space
+   long long int factorial(long long int number,long long int k){
+        if(number==0)
+            return k;
+        return factorial(number-1,k*number);
+    }
+
 };
 
 void main(){
@@ -16,5 +26,5 @@ void main(){
     cout << "Enter number: ";
     cin >> number;
     Solution sol;
-    cout << number << "! = "<<sol.factorial(number) << endl;
+    cout << number << "! = "<<sol.factorial(number,1) << endl;
 }
